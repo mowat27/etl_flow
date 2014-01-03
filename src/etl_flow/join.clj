3807cf8f533731ac5fd3ee97176080e65c9ae5f4
Adrian Mowat <adrian.mowat@gmail.com>
2014-01-03 14:40:36 +0000

    Added join

--- src/etl_flow/join.clj
+++ src/etl_flow/join.clj
@@ -1,1 +1,30 @@
-(ns etl-flow.join)
+(ns etl-flow.join
+  (:require [clojure.data :refer [diff]]
+            [clojure.set :as set]))
+
+(defn filter-index [idx ks]
+  (mapv (partial apply hash-map) (filter (fn [[kv _]] (contains? ks kv)) idx)))
+
+(defn group-by-key [& coll]
+  (apply merge-with (fn [x y] 
+                      (if (vector? x) 
+                        (conj x y) 
+                        [x y])) 
+         (map #(apply merge %) coll)))
+
+(defn unindex [idx] 
+  (vec (reduce #(set/union %1 (first %2)) #{} (mapv #(vals %) idx))))
+
+(defn product [coll] 
+  (->> (map (fn [[kv rows]] rows) coll)
+       (mapcat (fn [[x-rows y-rows]] 
+             (for [x x-rows y y-rows]
+               [x y])))
+       vec))
+
+(defn join [key old new & {:keys [unused-old-fn unused-new-fn match-fn] :as opts}]
+  (let [indexes (map #(set/index % key) [old new])
+        [unmatched-keys-0 unmatched-keys-1 matched-keys] (apply diff (map #(set (keys %)) indexes))]
+    {:matches  (product (apply group-by-key (map #(filter-index % matched-keys) indexes)))
+     :unused-0 (unindex (filter-index (first indexes) unmatched-keys-0))
+     :unused-1 (unindex (filter-index (second indexes) unmatched-keys-1))}))
--- test/etl_flow/t_join.clj
+++ test/etl_flow/t_join.clj
@@ -1,3 +1,28 @@
 (ns etl-flow.t-join
   (:require [midje.sweet :refer :all]
             [etl-flow.join :refer :all]))
+
+(def input-0 #{{:id "abc" :first "Jim" :last "McTavish"}
+               {:id "def" :first "Tam" :last "The bam"}
+               {:id "aaa" :first "foo"  :last "bar"}
+               {:id "xyz" :first "Davey" :last "Davis"}})
+
+(def input-1 #{{:id "abc" :first "James" :last "McTavish"}
+               {:id "abc" :first "Don"   :last "Williams"}
+               {:id "xyz" :first "Davey" :last "Davis"}
+               {:id "hij" :first "Wullie" :last "The daftie"}})
+
+(defn run []
+  (join [:id] input-0 input-1))
+
+(fact (:matches (run)) => 
+      [[{:last "Davis", :first "Davey", :id "xyz"} {:last "Davis", :first "Davey", :id "xyz"}] 
+       [{:last "McTavish", :first "Jim", :id "abc"} {:last "McTavish", :first "James", :id "abc"}] 
+       [{:last "McTavish", :first "Jim", :id "abc"} {:last "Williams", :first "Don", :id "abc"}]])
+
+(fact (:unused-0 (run)) => 
+      [{:last "The bam", :first "Tam", :id "def"} {:last "bar", :first "foo", :id "aaa"}])
+
+(fact (:unused-1 (run)) => 
+      [{:id "hij" :first "Wullie" :last "The daftie"}])
+
